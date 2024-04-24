069020bab7301854ce000425cc0ed2ca5a7ecddc
CyriB <167970830+CyriB@users.noreply.github.com>
2024-04-24 18:42:59 +0200

    Add files via upload
    V2, working

--- LDPC.m
+++ LDPC.m
@@ -1,22 +1,9 @@
 
-rate_error = []; % list of error rate depending on BER
-
-f_carrier = 2E9; % Hz
-T = 200E-9; % (duration of a symbol)
-
-
-nb_bits = 2^5; % Number of bits send 2^6=64
-bits_p_sym = 4; % number of bits per symbol, so the number of symbol is 512/8 = 64
-
 vect = [randi([0,1],5,1)';
     randi([0,1],5,1)';
     randi([0,1],5,1)';]; % random vector of bits
 
 
-% % Create initial parity check matrix of size 128 x 256
-% H0 = generate_ldpc(128, 256,0,1,3);
-% % Compute parity bits (128 x nb of packets) and generate final parity check matrix
-% [paritybits, H] = encode_ldpc(vect, H0, 0);
 
 H =[1 1 0 1 1 0 0 1 0 0;
     0 1 1 0 1 1 1 0 0 0;
@@ -24,13 +11,14 @@ H =[1 1 0 1 1 0 0 1 0 0;
     1 1 0 0 0 1 1 0 1 0;
     0 0 1 0 0 1 0 1 0 1];
 
+% H = rref(H)
+% mod(H,2)
 
-
-P =[0 1 1 1 0;
-    1 0 1 0 0;
-    1 0 1 0 1;
-    0 0 1 1 1;
-    1 1 0 0 1].'; 
+P =[0     1     1     1     0;
+    1     0     1     0     0;
+    1     0     1     0     1;
+    0     0     1     1     1;
+    1     1     0     0     1].'; 
 
 I_5 = eye(5);
 
@@ -38,123 +26,18 @@ Gen = [P I_5];
 
 vect_enc = vect * Gen;
 
-        
-vect_enc = vect_enc(:)';
+vect_enc = reshape(vect_enc',[1 size(vect_enc,1) * size(vect_enc,2)]);
 for m = 1:length(vect_enc)
-    if rem(vect_enc(m),2) == 0
-        vect_enc(m) = 0;
-    else
-        vect_enc(m) = 1; 
-    end
+    vect_enc(m) = mod(vect_enc(m),2);
+    
 end
-
-decoded = decoding(vect_enc,P,H);
-vect_verif = vect(:);
-
-
-
+vect_enc_with_error = vect_enc;
+vect_enc_with_error(25) = vect_enc_with_error(25)+1; % insert error
+vect_enc_with_error = mod(vect_enc_with_error,2);
+decoded = decoding(vect_enc_with_error,P,H);
 
 
 
-% 
-% vect_enc = [vect_enc,0,1];
-% 
-% 
-% 
-% 
-% for l = 1:length(vect_enc)
-%     if rem(vect_enc(l),2) == 0
-% 
-%         vect_enc(l) = 0;
-%     else
-% 
-%         vect_enc(l) = 1;
-% 
-%     end
-% 
-% end
-% 
-% vect_enc = vect_enc';
-% 
-% map = mapping(vect_enc,bits_p_sym,'qam');
-% ma2= [];
-% for i = map'
-%     ma2 = [ma2,0,0,0,0,i,0,0,0,0,0];
-% end
-% % demap = demapping(map,4,'qam');
-% % 64 bit per symbol
-% % 5 mega symbol per second
-% 
-% beta = 0.3 ; % (0<=beta<=1)
-% f = -25E6:25E4:25E6-25E4;
-% G = [];
-% for l = f
-%     G = [G,HalfrootNyquistFilter(T,beta,l)];
-% end
-% 
-% G = fftshift(G); % for the symetric
-% g = ifft(G);
-% g = ifftshift(g); % for the signal in positive timelin
-% 
-% 
-% s= conv(ma2,g,'same');
-% 
-% V = var(s) / 2; % variance of the signal
-% 
-% 
-% Eb = V * T  / bits_p_sym; % Calculation of a bit energy
-% 
-% 
-% nb_point_BER = -1.5:0.5:13; % BER that will be explore to draw curves
-% for k = nb_point_BER 
-%     % We adjust the noise to make the curve
-%     N0 = Eb / (10^(k/10));
-% 
-%     Y = sqrt(N0/2 * 50E6) .* (randn(1,length(s)) + 1i * randn(1,length(s)));  % Creating white gaussian noise
-%     t = s + Y; % Signal with added noise
-% 
-%     g_invert = conj(flip(g)); % flip function inverse the order of the vecteur g(-t). In fact g(-t) = g(t)
-%     r_10 = conv(t,g_invert,'same'); % convolution with g(-t). Caution with rate !
-%     r_10 = r_10 / max(conv(g,g)); %normalization
-% 
-% 
-%     % To go back to the intial sampling rate, we take one symbol over 10
-%     r=[];
-% 
-%     count = 6; % The fourth is the first to be sampled
-%     for l = r_10
-%         if rem(count, 10) == 0
-%             r = [r,l];
-% 
-%         end
-%         count = count + 1;
-%     end
-% 
-%     r = r';
-% 
-% 
-%     demap = demapping(r,bits_p_sym,'qam');
-% 
-% 
-%     % We count the number of error
-%     error_vct = vect_enc - demap;
-% 
-% 
-%     nb_error = 0;
-% 
-%     for l = error_vct'
-%         if l ~= 0
-%             nb_error = nb_error + 1;
-%         end
-%     end
-% 
-%     rate_error = [rate_error,nb_error/length(error_vct)];
-% 
-% end
-% 
-% 
-% %semilogy(nb_point_BER,rate_error)
-% 
-% 
-% 
-% 
+verif = decoded- vect_enc;
+
+
--- decoding.m
+++ decoding.m
@@ -1,96 +1,76 @@
 function [bits_stream] = decoding(stream_encoded,P,H)
 
-[k,n] = size(P);
-%H = [eye(n) P'];
+
 [k,n] = size(H);
-y = zeros(4,n);
-bits_stream_temp = zeros(size(stream_encoded));
+y = zeros(3,n);
+
+bits_stream_temp = zeros(length(stream_encoded)/n,n);
 
 % tanner
-for m = 1:length(stream_encoded) - n
+for m = 1:length(stream_encoded)/n 
     c = zeros(1,n);
     c_bis = zeros(2,n);
     for w = 1:n
-        c(w) = stream_encoded(m+w-1);
+        c(w) = stream_encoded((m-1)*n+w);
     end
+   
+    while_cond = 1;
+    loop = 0;
+    while while_cond == 1
+        loop = loop +1;
+        y(1,:) = c; % what we received
+        
     
-    % f = c * H';
-    % 
-    % % Modulo 2
-    % for l = 1:length(f)
-    %     if rem(f(l),2) == 0
-    %         f(l) = 0;
-    %     else
-    %         f(l) = 1; 
-    %     end
-    % end
-    % % end of modulo 2
-
-    y(1,:) = c; % what we received
-
-
-    for v = k
-        for w = 1:n
-            if H(v,w) ~=0 && c_bis(2,w) == 0 % To be sure that we went for the first time trhough c(w)
-                c_bis(2,w) = 1; % we have compute c(w) for the first time
-                c_bis(1,w) = c * H(v,:)' - c(w); % cancel the participation to himself
-                % modulo 2
-                if rem(c_bis(1,w),2) == 0
-                        c_bis(1,w) = 0;
-                    else
-                        c_bis(1,w) = 1; 
+        for v = 1:k
+            for w = 1:n
+                if H(v,w) ~=0 && c_bis(2,w) == 0 % To be sure that we went for the first time trhough c(w)
+                    c_bis(2,w) = 1; % we have compute c(w) for the first time
+                    c_bis(1,w) = c * H(v,:)' - c(w); % cancel the participation to himself
+                    c_bis(1,w) = mod(c_bis(1,w),2);           
                 end
             end
         end
-    end
-
-    y(2,:) = c_bis(1,:); 
-
-    for v = k
-        for w = 1:n
-           
-            c_bis(1,w) = c * H(v,:)' - c(w); % cancel the participation to himself
-            % modulo 2
-            if rem(c_bis(1,w),2) == 0
-                    c_bis(1,w) = 0;
-                else
-                    c_bis(1,w) = 1; 
+    
+        y(2,:) = c_bis(1,:); 
+    
+        for v = 1:k
+            for w = 1:n
+                if H(v,w) ~=0
+                    c_bis(1,w) = c * H(v,:)' - c(w); % cancel the participation to himself
+                    c_bis(1,w) = mod(c_bis(1,w),2);
+                end
             end
-            
         end
-    end
+    
+        y(3,:) = c_bis(1,:); 
+        
+        for w = 1:n
+            bits_stream_temp(m,w) = y(3,w) + y(2,w) + y(1,w);
+            if bits_stream_temp(m,w)>=2
+                bits_stream_temp(m,w) = 1; % majority of 1, so the decision is to put a 1
+            else
+                bits_stream_temp(m,w) = 0; % majority of 0, so the decision is to put a 0
+            end
+        end
+        
+        u = zeros(1,n);
+        for w = 1:n
+            u(w) = bits_stream_temp(m,w);
+        end
+    
+        s = u * H';
+        s = mod(s,2);
 
-    y(3,:) = c_bis(1,:); 
-    
-    for w = 1:n
-        bits_stream_temp(m+n-1) = y(3,n) + y(2,n) + y(1,n);
-        if bits_stream_temp(m+n-1)>=2
-            bits_stream_temp(m+n-1) = 1; % majority of 1, so the decision is to put a 1
-        else
-            bits_stream_temp(m+n-1) = 0; % majority of 0, so the decision is to put a 0
+        if nnz(s) == 0 || loop > 10
+            while_cond = 0;
+            c = u; % the information decided become the new information received for the next iteration
         end
-    end
-    
+
+
+    end % end of while loop
 
 end % end loop for m = 1:length(stream_encoded) - n
+bits_stream = reshape(bits_stream_temp',[1 size(bits_stream_temp,1) * size(bits_stream_temp,2)]);
 
-% decoding
-bits_stream = zeros(length(bits_stream_temp)/n,k);
-for w = 1:length(bits_stream_temp)/n
-    u = zeros(1,n);
-    for m = 1:n
-        u(m) = bits_stream_temp((w-1)*n+m);
-    end
-    s = u * H';
-    for m = 1:k
-        if rem(s(m),2) == 0
-            s(m) = 0;
-        else
-            s(m) = 1; 
-        end
-    end
-    bits_stream(w,:) = s;
-end
-bits_stream = bits_stream(:);
 
-end
+end % end of the decoding function
